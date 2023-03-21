8ac94b450f226a20530c998d3468096eb528f994
piero009 <127710886+piero009@users.noreply.github.com>
2023-03-21 09:46:35 +0700

    TUGAS

---
+++ coding/coding2.php
@@ -0,0 +1,27 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <title>PHP Dasar</title>
+</head>
+
+<body>
+    <h1>Belajar PHP Dasar</h1>
+    <?php
+    echo "Hello World";
+    ?>
+</body>
+
+<body>
+    <h1>Menggunakan Variable</h1>
+    <?php
+    $nim = "312110604";
+    $nama = 'Flandylan Rui';
+    $kelas = 'TI.21.A.1';
+    echo "Kelas : " . $kelas . "<br>";
+    echo "NIM : " . $nim . "<br>";
+    echo "Nama : $nama";
+    ?>
+</body>
+</html>
